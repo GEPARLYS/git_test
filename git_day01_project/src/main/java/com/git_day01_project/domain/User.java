6dec3be09f7dd6340919be7c5fc64faa61c3c28a
jcoder <847423815@qq.com>
2023-09-14 17:40:47 +0800

    new User.java commit localhost warehouse

---
+++ git_day01_project/src/main/java/com/git_day01_project/domain/User.java
@@ -0,0 +1,28 @@
+package com.git_day01_project.domain;
+
+/**
+ * @Author j
+ * @Date 2023/9/14 17:34 //TODO
+ * @Version 1.0
+ */
+public class User {
+    private Integer id;
+
+    private String username;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
